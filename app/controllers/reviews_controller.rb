71a701e3ccaad902a91b9758b0b83b9f1014aa6f
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2019-02-09 16:09:45 +0800

    added roots and all models working - needs user interface

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,34 @@
+class ReviewsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+     @review = Review.new(review_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    @review.save
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+ private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
